a7791c9b9db407f7cf7865a81a6176556ce0e2e6
@hexfa <amir.dehdarian2009@gmail.com>
2024-12-21 10:49:13 +0330

    [FIX_MERGE_ISSUE]

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -28,6 +28,7 @@ import 'package:todo/presentation/bloc/project/project_bloc.dart';
 import 'package:todo/presentation/route/app_router.dart';
 import 'package:todo/services/api/dio_client.dart';
 import 'package:todo/services/api/project_service.dart';
+
 import '../../domain/repositories/tasks_repository.dart';
 import '../../domain/usecases/create_project_usecase.dart';
 import '../../domain/usecases/get_tasks_usecase.dart';
@@ -75,18 +76,13 @@ Future<void> setupLocator(String token) async{
   getIt.registerLazySingleton<SyncLocalDataSource>(
       () => SyncLocalDataSource(getIt()));
 
-  // Register repositories
-  getIt.registerLazySingleton<ProjectsRepository>(() => ProjectsRepositoryImpl(
-      remoteDataSource: getIt(), localDataSource: getIt(), syncQueue: getIt()));
-  getIt.registerLazySingleton<CommentsRemoteDataSource>(
-        () => CommentsRemoteDataSourceImpl(
-      getIt<ProjectService>(),
-    ),
-  );
+
 
   //register repositories
   getIt.registerLazySingleton<ProjectsRepository>(
-          () => ProjectsRepositoryImpl(remoteDataSource:getIt<ProjectsRemoteDataSource>(), localDataSource: getIt<ProjectsLocalDataSource>(), syncQueue: getIt<SyncLocalDataSource>()));
+          () => ProjectsRepositoryImpl(remoteDataSource:getIt<ProjectsRemoteDataSource>(),
+              localDataSource: getIt<ProjectsLocalDataSource>(),
+              syncQueue: getIt<SyncLocalDataSource>()));
 
   getIt.registerLazySingleton<TasksRepository>(
     () => TasksRepositoryImpl(
@@ -94,7 +90,11 @@ Future<void> setupLocator(String token) async{
         localDataSource: getIt(),
         syncQueue: getIt()),
   );
-
+  getIt.registerLazySingleton<CommentsRemoteDataSource>(
+        () => CommentsRemoteDataSourceImpl(
+      getIt<ProjectService>(),
+    ),
+  );
   getIt.registerLazySingleton(() => SyncManager(
     syncQueue: getIt<SyncLocalDataSource>(),
     projectsRemoteDataSource: getIt<ProjectsRemoteDataSource>(),
